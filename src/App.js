c5413ddac35f38fb905bedae70b2ceec884f8f9d
sonei666888 <1144284812@qq.com>
2024-08-22 22:32:01 +0800

    240822_react-readux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-// 项目的根组件
-// App -> index.js -> public/index.html(root)
-function App() {
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+// 导入actionCreater
+import { inscrement, decrement, addToNum } from './store/modules/counterStore'
+import { fetchChannlList } from './store/modules/channelStore'
+function App () {
+  const { count } = useSelector(state => state.counter)
+  const { channelList } = useSelector(state => state.channel)
+  const dispatch = useDispatch()
+  // 使用useEffect触发异步请求执行
+  useEffect(() => {
+    dispatch(fetchChannlList())
+  }, [dispatch])
   return (
     <div className="App">
-      this is app
+      <button onClick={() => dispatch(decrement())}>-</button>
+      {count}
+      <button onClick={() => dispatch(inscrement())}>+</button>
+      <button onClick={() => dispatch(addToNum(10))}>add To 10</button>
+      <button onClick={() => dispatch(addToNum(20))}>add To 20</button>
+      <ul>
+        {channelList.map(item => <li key={item.id}>{item.name}</li>)}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-// 项目的入口 从这里开始运行
-// React必要的俩个核心包
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-// 导入项目的根组件
+import './index.css'
 import App from './App'
+import reportWebVitals from './reportWebVitals'
+import store from './store'
+import { Provider } from 'react-redux'
 
-// 把App根组件渲染到id为root的dom节点上
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
